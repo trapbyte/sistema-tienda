c8b84b36bffa94eb7abf514bbf51b04c72b31db6
Alejandro Gutiérrez Henao <124401970+trapbyte@users.noreply.github.com>
2025-11-21 14:05:57 -0500

    Set default dialect to "mysql" in Sequelize configuration

--- sistema-tienda/src/baseDatos/index.js
+++ sistema-tienda/src/baseDatos/index.js
@@ -22,7 +22,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT || "mysql",
+    logging: false
   }
 );
 
